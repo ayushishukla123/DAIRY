cda5a2a93bb94cf2206c5c01fb1afb85a958c862
Ayushi Shukla <ayushishukla@Ayushis-MacBook-Air.local>
2026-01-05 23:23:18 +0530

    Footer colour change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,49 +2,111 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
-    <div class="container py-5">
-        <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Our Office</h5>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                <div class="d-flex pt-3">
-                    <a class="btn btn-square btn-secondary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-square btn-secondary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square btn-secondary rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-square btn-secondary rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
-                </div>
+    <div
+      className="container-fluid footer mt-5 py-5 wow fadeIn"
+      data-wow-delay="0.1s"
+      style={{ backgroundColor: "#404A3D" }}
+    >
+      <div className="container py-5">
+        <div className="row g-5">
+          {/* Office */}
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Our Office</h5>
+            <p className="text-light mb-2">
+              <i className="fa fa-map-marker-alt me-3"></i>
+              123 Street, New York, USA
+            </p>
+            <p className="text-light mb-2">
+              <i className="fa fa-phone-alt me-3"></i>
+              +012 345 67890
+            </p>
+            <p className="text-light mb-2">
+              <i className="fa fa-envelope me-3"></i>
+              info@example.com
+            </p>
+
+            {/* Social Icons */}
+            <div className="d-flex pt-3">
+              {["twitter", "facebook-f", "youtube", "linkedin-in"].map(
+                (icon, i) => (
+                  <a
+                    key={i}
+                    href="#"
+                    className="btn btn-square rounded-circle me-2"
+                    style={{
+                      backgroundColor: "#EDDD5E",
+                      color: "#404A3D",
+                    }}
+                  >
+                    <i className={`fab fa-${icon}`}></i>
+                  </a>
+                )
+              )}
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Quick Links</h5>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Our Services</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
-                <a class="btn btn-link" href="">Support</a>
+          </div>
+
+          {/* Quick Links */}
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Quick Links</h5>
+            <a className="btn btn-link text-light" href="#">
+              About Us
+            </a>
+            <a className="btn btn-link text-light" href="#">
+              Contact Us
+            </a>
+            <a className="btn btn-link text-light" href="#">
+              Our Services
+            </a>
+            <a className="btn btn-link text-light" href="#">
+              Terms & Condition
+            </a>
+            <a className="btn btn-link text-light" href="#">
+              Support
+            </a>
+          </div>
+
+          {/* Business Hours */}
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Business Hours</h5>
+            <p className="text-light mb-1">Monday - Friday</p>
+            <h6 className="text-light">09:00 am - 07:00 pm</h6>
+            <p className="text-light mb-1">Saturday</p>
+            <h6 className="text-light">09:00 am - 12:00 pm</h6>
+            <p className="text-light mb-1">Sunday</p>
+            <h6 className="text-light">Closed</h6>
+          </div>
+
+          {/* Newsletter */}
+          <div className="col-lg-3 col-md-6">
+            <h5 className="text-white mb-4">Newsletter</h5>
+            <p className="text-light">
+              Dolor amet sit justo amet elitr clita ipsum elitr est.
+            </p>
+
+            <div className="position-relative w-100">
+              <input
+                className="form-control bg-transparent text-light w-100 py-3 ps-4 pe-5"
+                type="text"
+                placeholder="Your email"
+              />
+
+              {/* SignUp Button */}
+              <button
+                type="button"
+                style={{
+                  backgroundColor: "#EDDD5E",
+                  color: "#404A3D",
+                  border: "none",
+                }}
+                className="btn py-2 position-absolute top-0 end-0 mt-2 me-2"
+              >
+                SignUp
+              </button>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Business Hours</h5>
-                <p class="mb-1">Monday - Friday</p>
-                <h6 class="text-light">09:00 am - 07:00 pm</h6>
-                <p class="mb-1">Saturday</p>
-                <h6 class="text-light">09:00 am - 12:00 pm</h6>
-                <p class="mb-1">Sunday</p>
-                <h6 class="text-light">Closed</h6>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Newsletter</h5>
-                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                <div class="position-relative w-100">
-                    <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email"/>
-                    <button type="button" class="btn btn-secondary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,90 +6,98 @@ import Testimonial from "./Testimonial";
 function Home() {
   return (
     <>
-    <div className="container-fluid px-0 mb-5">
-      <div
-        id="header-carousel"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          {/* Slide 1 */}
-          <div className="carousel-item active">
-            <img className="w-100" src="/img/carousel-1.jpg" alt="Dairy Farm" />
-            <div className="carousel-caption">
-              <div className="container">
-                <div className="row justify-content-start">
-                  <div className="col-lg-8 text-start">
-                    <p className="fs-4 text-white">Welcome to our dairy farm</p>
-                    <h1 className="display-1 text-white mb-5">
-                      The Farm of Dairy Products
-                    </h1>
-                    <Link
-                      to="/products"
-                      className="btn btn-secondary rounded-pill py-3 px-5"
-                    >
-                      Explore More
-                    </Link>
+      <div className="container-fluid px-0 mb-5">
+        <div
+          id="header-carousel"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {/* Slide 1 */}
+            <div className="carousel-item active">
+              <img
+                className="w-100"
+                src="/img/carousel-1.jpg"
+                alt="Dairy Farm"
+              />
+              <div className="carousel-caption">
+                <div className="container">
+                  <div className="row justify-content-start">
+                    <div className="col-lg-8 text-start">
+                      <p className="fs-4 text-white">
+                        Welcome to our dairy farm
+                      </p>
+                      <h1 className="display-1 text-white mb-5">
+                        The Farm of Dairy Products
+                      </h1>
+                      <Link
+                        to="/products"
+                        className="btn btn-secondary rounded-pill py-3 px-5"
+                      >
+                        Explore More
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Slide 2 */}
+            <div className="carousel-item">
+              <img
+                className="w-100"
+                src="/img/carousel-2.jpg"
+                alt="Organic Dairy"
+              />
+              <div className="carousel-caption">
+                <div className="container">
+                  <div className="row justify-content-end">
+                    <div className="col-lg-8 text-end">
+                      <p className="fs-4 text-white">
+                        Welcome to our dairy farm
+                      </p>
+                      <h1 className="display-1 text-white mb-5">
+                        Best Organic Dairy Products
+                      </h1>
+                      <Link
+                        to="/products"
+                        className="btn btn-secondary rounded-pill py-3 px-5"
+                      >
+                        Explore More
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          {/* Slide 2 */}
-          <div className="carousel-item">
-            <img
-              className="w-100"
-              src="/img/carousel-2.jpg"
-              alt="Organic Dairy"
-            />
-            <div className="carousel-caption">
-              <div className="container">
-                <div className="row justify-content-end">
-                  <div className="col-lg-8 text-end">
-                    <p className="fs-4 text-white">Welcome to our dairy farm</p>
-                    <h1 className="display-1 text-white mb-5">
-                      Best Organic Dairy Products
-                    </h1>
-                    <Link
-                      to="/products"
-                      className="btn btn-secondary rounded-pill py-3 px-5"
-                    >
-                      Explore More
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {/* Controls */}
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-
-        {/* Controls */}
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
-     <About />
-     <Products />
-     <Testimonial />
-   </>
+      <Products />
+      <About />
+      <Testimonial />
+    </>
   );
 }
 
